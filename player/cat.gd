30ecc4e77e6faf219d8c5e8fbe53a12bbd12e896
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-15 08:55:14 -0500

    Day 6

--- player/cat.gd
+++ player/cat.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var cat_animation_player: AnimationPlayer = $cat_v2/AnimationPlayer
 
 
 const SPEED: float = 5.0
@@ -14,12 +15,17 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready() -> void:
 	animation_player.play("start")
 	await animation_player.animation_finished
+	cat_animation_player.play("hind_legs")
+	await get_tree().create_timer(1.0).timeout
+	cat_animation_player.stop()
+	cat_animation_player.play("belly_crawl")
+	rotate_x(deg_to_rad(90))
 	is_climbing = true
 	
 
 func _physics_process(delta: float) -> void:
 	# Fixes every other run failing after about 25 tower pieces
-	position.z = 5
+	position.z = 5.5
 	
 	# Climb kitty climb!
 	if is_climbing:
